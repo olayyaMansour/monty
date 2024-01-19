f8dd6e3f25a3164c918998dda348475ca940a4cd
olayyaMansour <mansour.olayya@gmail.com>
2024-01-18 18:29:11 -0800

    add task 5

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * nop - Do nothing
+ * @stack: Double pointer to the head of the stack
+ * @line_number: Line number in the Monty bytecode file
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+
