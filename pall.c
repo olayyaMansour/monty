c9cf7a0bc321966a93eb4c0af20b6849e19aae0e
olayyaMansour <mansour.olayya@gmail.com>
2024-01-18 16:57:58 -0800

    test task 1

---
+++ pall.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+
---
+++ push.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *token;
+	int value;
+	token = strtok(NULL, " \t\n");
+	if (token == NULL || !isdigit(*token))
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	value = atoi(token);
+	stack_t *new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = value;
+	new_node->prev = NULL;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+	new_node->next = *stack;
+	*stack = new_node;
+}
+
