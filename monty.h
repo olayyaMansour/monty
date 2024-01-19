08e792af434c5721eec86be7183991eebb1ef4c6
olayyaMansour <mansour.olayya@gmail.com>
2024-01-18 18:20:36 -0800

    add task 2

--- monty.h
+++ monty.h
@@ -40,5 +40,7 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+
 #endif /* MONTY_H */
 
