dd6892efda29c6ec65dcf1ece78dbef84219ef1e
olayyaMansour <mansour.olayya@gmail.com>
2024-01-18 18:36:26 -0800

    update file

--- monty_main.c
+++ monty_main.c
@@ -1,57 +1,53 @@
 #include "monty.h"
-#include <stdio.h>
-/**
- * MAX_LINE_LENGTH - Maximum length of a line in the Monty bytecode file
- * Description: This constant defines the maximum number of characters
- * allowed in a line of the Monty bytecode file.
- */
-#define MAX_LINE_LENGTH 1024
-/**
- * main - Entry point for the Monty interpreter
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
- */
+
 int main(int argc, char *argv[])
 {
 	FILE *file;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
 	stack_t *stack = NULL;
-	char line[MAX_LINE_LENGTH];
 	unsigned int line_number = 0;
-	char *opcode;
 	void (*op_func)(stack_t **, unsigned int) = NULL;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
+
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
-	while (fgets(line, sizeof(line), file) != NULL)
+
+	while ((read = getline(&line, &len, file)) != -1)
 	{
 		line_number++;
-		opcode = strtok(line, " \t\n");
-		if (opcode == NULL)
+
+		char *opcode = strtok(line, " \t\n");
+		if (opcode == NULL || opcode[0] == '#')
 			continue;
-		op_func = NULL;
-		if (strcmp(opcode, "push") == 0)
-			op_func = push;
-		else if (strcmp(opcode, "pall") == 0)
-			op_func = pall;
+
+		op_func = get_op_func(opcode);
 		if (op_func == NULL)
 		{
 			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+			free_stack(&stack);
+			free(line);
 			fclose(file);
 			exit(EXIT_FAILURE);
 		}
+
 		op_func(&stack, line_number);
 	}
 
+	free_stack(&stack);
+	free(line);
 	fclose(file);
-	return (EXIT_SUCCESS);
+
+	exit(EXIT_SUCCESS);
 }
+
